7c39cd55c7ffade45ea54f01a95e0bd64248afed
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-03-19 12:51:14 +0000

    User can dislike
    Co-authored-by: Nikhil Vijayan <nkhilv@gmail.com>
    Co-authored-by: Jiseong Park <jpebs2016@gmail.com>
    Co-authored-by: Krzysztof Balejko <chrisbalejko86@gmail.com>
    Co-authored-by: Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
    Co-authored-by: Ibrahim Butt <ibrahimbutt@me.com>

--- src/LookPage/index.js
+++ src/LookPage/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { X } from 'react-feather'
 
 const LookImage = ({ src, className }) => {
   return <img className={className} id="displayed-look" src={src} />
@@ -15,8 +16,26 @@ const StyledLookImage = styled(LookImage)`
   box-shadow: 0px 4px 20px rgba(79, 79, 79, 0.25);
 `
 
+const DismissButton = ({IconComponent, className}) => {
+  return <div className={className} id="dismiss-button">{<IconComponent color="#EE8CA3" size="32"/>}</div>
+}
+const StyledDismissButton = styled(DismissButton)`
+  display: inline-grid;
+  margin: 0 auto;
+  box-shadow: 0px 4px 20px rgba(79, 79, 79, 0.25);
+  border-radius: 100px;
+  padding: 10px;
+`
+
+
 const LookPage = () => {
-  return <StyledLookImage src="https://i.imgur.com/n1IqG2c.jpg" />
+  return (
+    <div>
+      {console.log(X)}
+      <StyledLookImage src="https://i.imgur.com/n1IqG2c.jpg" />
+      <StyledDismissButton IconComponent={X} />
+    </div>
+  )
 }
 
 export default LookPage
