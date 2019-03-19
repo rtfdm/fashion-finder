6cdb04b4935bf354d395692fceda83ec7e2e278a
Ibrahim Butt <ibrahimbutt@me.com>
2019-03-19 10:49:37 +0000

    Look is displayed
    Co-authored-by: Nikhil Vijayan <nkhilv@gmail.com>
    Co-authored-by: Jiseong Park <jpebs2016@gmail.com>
    Co-authored-by: Krzysztof Balejko <chrisbalejko86@gmail.com>
    Co-authored-by: Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import LookPage from '../LookPage'
+
 export default () => {
-  return <h1>Hello world</h1>
+  return <LookPage />
 }
---
+++ src/LookPage/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class LookPage extends Component {
+  render() {
+    return <img id="displayed-look" src="https://i.imgur.com/n1IqG2c.jpg" />
+  }
+}
