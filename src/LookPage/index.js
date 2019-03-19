e2cb25669d4141eb24c214b753914cf53d8d3192
Ibrahim Butt <ibrahimbutt@me.com>
2019-03-19 11:35:04 +0000

    Add LookPage and style LookImage
    Co-authored-by: Nikhil Vijayan <nkhilv@gmail.com>
    Co-authored-by: Jiseong Park <jpebs2016@gmail.com>
    Co-authored-by: Krzysztof Balejko <chrisbalejko86@gmail.com>
    Co-authored-by: Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>

--- src/LookPage/index.js
+++ src/LookPage/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
-export default class LookPage extends Component {
-  render() {
-    return <img id="displayed-look" src="https://i.imgur.com/n1IqG2c.jpg" />
-  }
+const LookImage = ({ src, className }) => {
+  return <img className={className} id="displayed-look" src={src} />
 }
+
+const StyledLookImage = styled(LookImage)`
+  display: block;
+  margin: 0 auto;
+  margin-top: 100px;
+  width: 400px;
+  height: 400px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 20px rgba(79, 79, 79, 0.25);
+`
+
+const LookPage = () => {
+  return <StyledLookImage src="https://i.imgur.com/n1IqG2c.jpg" />
+}
+
+export default LookPage
