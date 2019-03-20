f04fee820ca13def28240e6815af994d3f75e8a1
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-20 16:43:43 +0000

    :alembic: sample basket items

---
+++ src/Basket/sampleBasketItems.js
@@ -0,0 +1,37 @@
+const basketItems = {
+  item1: {
+    name: 'Santa Cruz Cooper zip through crew neck',
+    image: '',
+    desc: `Known for its elaborately illustrated decks, \
+    skateboard brand Santa Cruz's accompanying apparel \
+    is every bit as cool as its cruisers. The result is \
+    a collection of T-shirts, hoodies and coach jackets, \
+    all kitted out with the label’s signature graphics \
+    and pop-art logo prints.`,
+    price: 1999,
+    size: 'M',
+    status: 'available',
+  },
+  item2: {
+    name: 'New Look corduroy jacket with borg lining in black',
+    image: '',
+    desc: `New Look's menswear collection takes influences \
+    from classic rebel movies and the best of effortless \
+    style.`,
+    price: 6550,
+    size: 'XL',
+    status: 'available',
+  },
+  item3: {
+    name: 'River Island crochet blazer in navy',
+    image: '',
+    desc: `High-street favourites River Island are known for \
+    adding unique touches to their directional menswear pieces.  \
+    Expect a complete range, covering everything from the newest \
+    denim cuts to graphic print T-shirts and knitwear.`,
+    price: 8550,
+    size: 'L',
+    status: 'available',
+  }
+}
+export default basketItems
