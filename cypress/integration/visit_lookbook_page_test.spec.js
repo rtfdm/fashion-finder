53838359e83f67cdbec48bce39835d3089b4bced
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-20 14:43:23 +0000

    :white_check_mark: lookbook link test

--- cypress/integration/visit_lookbook_page_test.spec.js
+++ cypress/integration/visit_lookbook_page_test.spec.js
@@ -1,9 +1,16 @@
 describe('Navigation between look page and basket', () => {
   it('can direct the user from look page to basket', () => {
     cy.visit('http://localhost:1234/')
-    console.log(cy.url())
     cy.get('#lookbook').click()
-    console.log(cy.url())
     cy.url().should('eq', 'http://localhost:1234/lookbook')
   })
+
+  it('can direct the user from look page to basket', () => {
+    cy.visit('http://localhost:1234/')
+    cy.get('#lookbook').click()
+    cy.get('#lookpage').click()
+    cy.url().should('eq', 'http://localhost:1234/')
+  })
+
+
 })
--- src/Basket/index.js
+++ src/Basket/index.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import { ArrowLeft } from 'react-feather'
+import { Link } from 'react-router-dom'
 
+const LookPageLink = ({ className, id, IconComponent, color }) => {
+  return (
+    <Link to="/">
+      <div className={className} id={id}>
+        Get inspired <nbsp />
+        {<IconComponent color={color} size="20" />}
+      </div>
+    </Link>
+  )
+}
 
 const Basket = () => {
   return (
     <div>
+      <LookPageLink id="lookpage" IconComponent={ArrowLeft} color="#EE8CA3" />
       <h1> Hello World </h1>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Basket
