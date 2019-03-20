33f1a0cfd761eefc2e59eddc1f4d6a4d0cc8efa6
Kai <notkaiho@gmail.com>
2019-03-20 18:19:51 +0000

    :hankey: added state to basket screen, renders UL

--- src/Basket/index.js
+++ src/Basket/index.js
@@ -2,9 +2,10 @@ import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import { ArrowLeft } from 'react-feather'
 import { Link } from 'react-router-dom'
-import basketItems from './sampleBasketItems'
+// import basketItems from './sampleBasketItems'
+import basketItems from './sampleitems'
 
-const MyBasket = basketItems
+// const MyBasket = basketItems
 
 const LookPageLink = ({ className, id, IconComponent, color }) => {
   return (
@@ -23,22 +24,31 @@ const ArrowMargin = {
 
 const StyledLookPageLink = styled(LookPageLink)``
 
-const Basket = () => {
+class Basket extends Component {
+  state = { MyBasket: {} }  
+  componentDidMount() {
+    this.setState( { MyBasket: basketItems })
+  }
+  render() {
   return (
     <div>
       <StyledLookPageLink id="lookpage" IconComponent={ArrowLeft} color="#EE8CA3" />
-      <h1> Hello World </h1>
+      <h1> Your Lookbook </h1>
+      {console.log(this.state.MyBasket)}
       <ul>
-      {Object.entries(MyBasket).map( (item, index) => {
-        return (
-          <Fragment key={index}>
-            <li>{item[1].name} £ {item[1].price}</li>
-          </Fragment>
-        )
-      })}
+      { Object.keys(this.state.MyBasket).map(key => {
+        
+        return <li key = {key}>
+            {this.state.MyBasket[key].name}  
+            {this.state.MyBasket[key].price}
+          
+        </li>
+       console.log(this.state.MyBasket[key]) }
+) }
       </ul>
     </div>
   )
+  }
 }
 
 
