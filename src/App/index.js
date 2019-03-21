eb3b8ef71b27d6e4729e19a1052724972646fc21
Ibrahim Butt <ibrahimbutt@me.com>
2019-03-21 11:11:04 +0000

    Change LookPage to be responsive
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>
    Co-authored-by: Jiseong Park <jpebs2016@gmail.com>

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
-import {createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 import 'normalize.css'
-
 
 import LookPage from '../LookPage'
 import LookInfo from '../LookInfo'
@@ -15,17 +14,24 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
+
+  #app {
+    @media only screen and (min-width: 400px) {
+      min-height: 100vh;
+      display: grid;
+      grid-template-columns: min-content;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 `
 
 export default () => (
   <BrowserRouter>
-  <div>
-    <GlobalStyles />
-    <Route exact={true} path="/" render={props => <LookPage />} />
-    <Route exact={true} path="/lookinfo" render={props => <LookInfo />} />
-  </div>
+    <div>
+      <GlobalStyles />
+      <Route exact={true} path="/" render={props => <LookPage />} />
+      <Route exact={true} path="/lookinfo" render={props => <LookInfo />} />
+    </div>
   </BrowserRouter>
-) 
-
-
-
+)
