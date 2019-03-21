29e9f16b4a9db0e75666effa844ae693d738c729
Kai <notkaiho@gmail.com>
2019-03-21 17:09:52 +0000

    Added delete item button to lookbook page
    Co-authored-by: Nikhil Vijayan <nkhilv@gmail.com>
    Co-authored-by: Krzysztof Balejko <chrisbalejko86@gmail.com>

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
+import { XCircle } from 'react-feather'
+
 
 const ListImageStyle = {
   borderRadius: '5px',
   maxWidth: '100%',
 }
 
-const ListItem = ({ className, src, name, desc, price, size, status }) => {
+const ListItem = ({ className, src, name, desc, price, size, status, DeleteButton }) => {
   return (
     <li className={className}>
       <img src={src} alt={name} style={ListImageStyle} />
@@ -15,6 +17,7 @@ const ListItem = ({ className, src, name, desc, price, size, status }) => {
       <p>Price: {price}</p>
       <p>Size: {size}</p>
       <p>Status: {status} </p>
+      <DeleteButton/>
     </li>
   )
 }
@@ -25,6 +28,27 @@ const StyledListItem = styled(ListItem)`
   padding: 10px;
   box-shadow: 0px 4px 20px rgba(79, 79, 79, 0.25);
   border-radius: 5px;
+  position: relative;
+`
+
+const DeleteButton = ({ className, id, IconComponent, color }) => {
+  return (
+    <div className={className} id={id}>
+      {<IconComponent color={color} size="32" />}
+    </div>
+  )
+}
+
+const StyledDeleteButton = styled(DeleteButton)`
+  background: rgba(255,255,255);
+  display: flex;
+  border-radius: 100px;
+  max-height: 35px;
+  max-width: 35px;
+  padding: 0px;
+  position: absolute;
+  right: 5px;
+  top: 8px;
 `
 
 class Item extends Component {
@@ -32,6 +56,7 @@ class Item extends Component {
     const { name, image, desc, price, size, status } = this.props.details
     return (
       <Fragment>
+         
         <StyledListItem
           src={image}
           name={name}
@@ -39,6 +64,7 @@ class Item extends Component {
           price={price}
           size={size}
           status={status}
+          DeleteButton={(props) => <StyledDeleteButton IconComponent={XCircle} color="#EE8CA3" id={name}/>}
         />
       </Fragment>
     )
