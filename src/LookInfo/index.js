70c4bdc75503771c5375f32372fcf7dc9765c562
Ibrahim Butt <ibrahimbutt@me.com>
2019-03-21 10:13:29 +0000

    Refactor LookPage and add text to back button
    Co-authored-by: Jiseong Park <jpebs2016@gmail.com>
    Co-authored-by: Kwaku Ankobia <kwaku.ankobia@gmail.com>

--- src/LookInfo/index.js
+++ src/LookInfo/index.js
@@ -2,17 +2,28 @@ import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import { ArrowLeft } from 'react-feather'
 
-const LookImage = ({ src, className}) => {
-  return <img className={className} id="look-image" src={src} />
-}
-
-const Button = styled.button `
+const Button = styled.button`
   background-color: transparent;
   border-color: transparent;
+  display: grid;
+  grid-template-columns: min-content min-content;
+  grid-column-gap: 10px;
+  align-items: center;
+  font-size: 18px;
+  color: #838383;
+  padding: 0;
 `
 
 const BackButton = ({ IconComponent }) => {
-  return <Button id="button">{ <IconComponent/> }</Button>
+  return (
+    <Button id="button">
+      {<IconComponent size="24" color="#838383" />} Back
+    </Button>
+  )
+}
+
+const LookImage = ({ src, className }) => {
+  return <img className={className} id="look-image" src={src} />
 }
 
 const StyledLookImage = styled(LookImage)`
@@ -21,11 +32,11 @@ const StyledLookImage = styled(LookImage)`
   box-shadow: 0px 4px 20px rgba(79, 79, 79, 0.25);
 `
 
-const Heading = styled.h2 `
+const Heading = styled.h2`
   font-size: 16px;
   font-weight: bold;
   margin-bottom: 15px;
-  color: #3B3B3B;
+  color: #3b3b3b;
 
   :not(:first-of-type) {
     margin-top: 30px;
@@ -37,7 +48,7 @@ const Paragraph = styled.p`
   margin-bottom: 10px;
   line-height: 150%;
   font-weight: normal;
-  color: #3B3B3B;
+  color: #3b3b3b;
 `
 
 const Price = styled.span`
@@ -55,28 +66,31 @@ const StyledBackButton = styled(BackButton)`
 `
 
 const LookDetails = () => {
-  return ( 
+  return (
     <div>
       <Heading>Brands</Heading>
       <Paragraph>Ralph Lauren</Paragraph>
 
       <Heading>Description </Heading>
       <Paragraph>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.Lorem Ipsum has been the industry 's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
       </Paragraph>
       <Price>£299.99</Price>
     </div>
-  ) 
+  )
 }
 
 const StyledLookDetails = styled(LookDetails)`
-  color: #3B3B3B;
+  color: #3b3b3b;
 `
 
 const Container = styled.div`
   width: min-content;
   margin: 0 auto;
-  padding-top: 30px;
+  padding-top: 50px;
 `
 
 const InnerContainer = styled.div`
@@ -90,15 +104,13 @@ const InnerContainer = styled.div`
 const LookInfoPage = () => {
   return (
     <Container>
-      <StyledBackButton IconComponent={ArrowLeft}/>
+      <StyledBackButton IconComponent={ArrowLeft} />
       <InnerContainer>
-        <StyledLookImage src='https://i.imgur.com/n1IqG2c.jpg' />
+        <StyledLookImage src="https://i.imgur.com/n1IqG2c.jpg" />
         <LookDetails />
       </InnerContainer>
     </Container>
   )
 }
-
-
 
 export default LookInfoPage
