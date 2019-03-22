cc824f2f8b6883d668862854855d450b222fc8fc
Nikhil Vijayan <nkhilv@gmail.com>
2019-03-22 16:11:06 +0000

    :lipstick: Add ul styling to GlobalStyles

--- src/App/index.js
+++ src/App/index.js
@@ -16,6 +16,10 @@ const GlobalStyles = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
+  ul {
+    padding: 0;
+  }
+
   #app {
     @media only screen and (min-width: 400px) {
       min-height: 100vh;
