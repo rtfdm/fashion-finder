f09b5ace1c4422e5aa9e2ca582e439864461fa03
Kai <notkaiho@gmail.com>
2019-03-25 16:05:45 +0000

    Added config and db connection to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+var config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+}
 firebase.initializeApp(config)
       const db = firebase.firestore()
 ReactDOM.render(<App />, document.getElementById('app'))
